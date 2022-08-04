bb5ee86d1a8b344f91f8a51ae6aa27f49a220eac
Scott Martin <scott@Scotts-MacBook-Pro.local>
2022-08-04 13:22:45 -0500

    Add scala format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,11 @@
 import authentication.Authentication
 import authentication.Authentication.AuthServiceEnv
-import repository.{Database, Repository, TodoRepository, UserCredentialRepository}
+import repository.{
+  Database,
+  Repository,
+  TodoRepository,
+  UserCredentialRepository
+}
 import zhttp.http.{Http, HttpApp, Method, Request, Response, _}
 import zhttp.service.Server
 import zio._
@@ -30,16 +35,27 @@ object Main extends App {
    */
 
   val login = Http.collectZIO[Request] {
-    case req @ Method.POST -> !! / "login" => log.info(s"Login request") *> bodyParser[UserCredential](req) >>= Authentication.createJwt
+    case req @ Method.POST -> !! / "login" =>
+      log.info(s"Login request") *> bodyParser[UserCredential](
+        req
+      ) >>= Authentication.createJwt
   }
 
   // NOTE: Could use #!# as an alias for *> basically or come up with another operator if we need something more specific
   def app(implicit jwt: JwtClaim) = Http.collectZIO[Request] {
-    case Method.GET -> !! / "todo" => roles("admin" or "supervisor") *> TodoController.getAll
+    case Method.GET -> !! / "todo" =>
+      roles("admin" or "supervisor") *> TodoController.getAll
     case Method.GET -> !! / "todo" / long(id) => TodoController.getById(id)
-    case Method.DELETE -> !! / "todo" / long(id) => roles("admin") *> roles("admin" or "") *> TodoController.delete(id)
-    case req @ Method.POST -> !! / "todo" => roles("admin" or "supervisor") *> bodyParser[Todo](req) >>= TodoController.create
-    case req @ Method.PATCH -> !! / "todo" / long(id) => roles("admin") *> bodyParser[Todo](req).flatMap(TodoController.update(id, _)) // NOTE: I'm not sure why but >>= w/ the the partial application fails here
+    case Method.DELETE -> !! / "todo" / long(id) =>
+      roles("admin") *> roles("admin" or "") *> TodoController.delete(id)
+    case req @ Method.POST -> !! / "todo" =>
+      roles("admin" or "supervisor") *> bodyParser[Todo](
+        req
+      ) >>= TodoController.create
+    case req @ Method.PATCH -> !! / "todo" / long(id) =>
+      roles("admin") *> bodyParser[Todo](req).flatMap(
+        TodoController.update(id, _)
+      ) // NOTE: I'm not sure why but >>= w/ the the partial application fails here
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
@@ -47,10 +63,14 @@ object Main extends App {
     // This uses authentication as a separate httpapp which gives you the jwt after...could also try out
     // the auth Middleware, but I'm not sure the jwtclaim gets passed to the downstream app then...and _I think_ we need
     // that to do role-specific authorizations since I don't think the middleware will know the matching path yet
-    val finalApp = login ++ Authentication.authenticationApp(Http.forbidden("None shall pass"), app(_))
+    val finalApp = login ++ Authentication.authenticationApp(
+      Http.forbidden("None shall pass"),
+      app(_)
+    )
 
     // ## Useful thing to remember app1 <> app2 means if app1 fails then app2 handles it (might be good for general error catching instead of exceptionHandler at some point)
-    val server = Server.start(8080, finalApp.catchAll(exceptionHandler) @@ debug)
+    val server =
+      Server.start(8080, finalApp.catchAll(exceptionHandler) @@ debug)
 
     // ZIO 1 way (alright....I can see why zio-magic is nicer)
 //    val config = DatabaseConfig.live ++ WebConfig.live ++ Blocking.live
@@ -77,7 +97,7 @@ object Main extends App {
       clock.Clock.live,
       UserCredentialRepository.live,
       Authentication.live,
-      ApplicationConfig.WebConfig.live,
+      ApplicationConfig.WebConfig.live
     )
 
     magic.exitCode

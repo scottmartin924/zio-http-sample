f339fa1d136c73430485c88d19ff73fb48068896
Scott Martin <scott@onedrop.today>
2022-09-16 20:26:31 -0500

    Idk this is just a sample

--- src/main/scala/authentication/Authentication.scala
+++ src/main/scala/authentication/Authentication.scala
@@ -22,29 +22,49 @@ object Authentication {
   private val AUTH_HEADER = "X-Access-Token"
   private val algo = JwtAlgorithm.HS512
 
-
-  def authenticationApp[R, E](fail: HttpApp[R, E], success: JwtClaim => HttpApp[R, E]): HttpApp[R with AuthServiceEnv, E] = {
-    Http.fromFunctionZIO[Request] { request =>
-      val job = for {
-        (_, token) <- ZIO.fromOption(request.getHeader(AUTH_HEADER)).orElseFail(new Exception(s"Missing or expired $AUTH_HEADER")) // Probably custom exception would be better
-        jwt <- Authentication.decode(token.toString)
-      } yield jwt
-      job.fold(_ => fail, success)
-    }.flatten
+  def authenticationApp[R, E](
+      fail: HttpApp[R, E],
+      success: JwtClaim => HttpApp[R, E]
+  ): HttpApp[R with AuthServiceEnv, E] = {
+    Http
+      .fromFunctionZIO[Request] { request =>
+        val job = for {
+          (_, token) <- ZIO
+            .fromOption(request.getHeader(AUTH_HEADER))
+            .orElseFail(
+              new Exception(s"Missing or expired $AUTH_HEADER")
+            ) // Probably custom exception would be better
+          jwt <- Authentication.decode(token.toString)
+        } yield jwt
+        job.fold(_ => fail, success)
+      }
+      .flatten
   }
 
-  def roles(roleQuery: RoleQuery)(implicit jwt: JwtClaim): RIO[AuthServiceEnv, JwtClaim] = {
+  def roles(
+      roleQuery: RoleQuery
+  )(implicit jwt: JwtClaim): RIO[AuthServiceEnv, JwtClaim] = {
     for {
       tokenRoles <- Authentication.getRoles(jwt)
-      _ <- if (roleQuery.isEligible(tokenRoles)) Task.succeed(jwt)
-            else Task.fail(BusinessException(Status.FORBIDDEN, code = "insufficient-permissions", s"Must satisfy the role requirement: $roleQuery"))
+      _ <-
+        if (roleQuery.isEligible(tokenRoles)) Task.succeed(jwt)
+        else
+          Task.fail(
+            BusinessException(
+              Status.FORBIDDEN,
+              code = "insufficient-permissions",
+              s"Must satisfy the role requirement: $roleQuery"
+            )
+          )
     } yield jwt
   }
 
   // Note: I'm no longer convinced this is useful...might as well just use zio operators unless we need something special
   type RoleCheckResponse = RIO[AuthServiceEnv, JwtClaim]
   implicit class RoleBuilder(roleCheck: RoleCheckResponse) {
-    def #!#[R](appAction: ZIO[R, Throwable, Response]): RIO[R with AuthServiceEnv, Response] = {
+    def #!#[R](
+        appAction: ZIO[R, Throwable, Response]
+    ): RIO[R with AuthServiceEnv, Response] = {
       roleCheck *> appAction
     }
   }
@@ -57,23 +77,39 @@ object Authentication {
     def getRoles(jwt: JwtClaim): Task[List[String]]
   }
 
-  class Service(authRepo: UserCredentialRepository.Service, config: WebConfig) extends AuthenticationService {
-    implicit val clock = java.time.Clock.systemUTC() // Could try to pull this out and use a zio clock, but not a big deal
+  class Service(authRepo: UserCredentialRepository.Service, config: WebConfig)
+      extends AuthenticationService {
+    implicit val clock =
+      java.time.Clock
+        .systemUTC() // Could try to pull this out and use a zio clock, but not a big deal
 
     override def createJwt(credentials: UserCredential): Task[Response] = for {
-      user <- authRepo.login(credentials).someOrFail(BusinessException(Status.FORBIDDEN, code = "invalid-credentials", details = s"User credentials invalid"))
+      user <- authRepo
+        .login(credentials)
+        .someOrFail(
+          BusinessException(
+            Status.FORBIDDEN,
+            code = "invalid-credentials",
+            details = s"User credentials invalid"
+          )
+        )
       roles <- authRepo.roles(user.userId.get)
       jwt <- encode(credentials.username, roles)
     } yield Response.json(s"""{"jwt": "$jwt"}""")
 
     override def encode(user: String, roles: List[String]): Task[String] = {
       // Looks like this jwt library issuedNow requires an implicit java clock :(. Could maybe override that w/ zio
-      val claim = JwtClaim(s"""{"user": "$user", "roles": ${roles.asJson.noSpaces}}""").issuedNow.expiresIn(config.tokenDuration)
-      Task.succeed(Jwt.encode(claim, config.secret, algo)) // Not sure this really a succeed
+      val claim = JwtClaim(
+        s"""{"user": "$user", "roles": ${roles.asJson.noSpaces}}"""
+      ).issuedNow.expiresIn(config.tokenDuration)
+      Task.succeed(
+        Jwt.encode(claim, config.secret, algo)
+      ) // Not sure this really a succeed
     }
 
     override def decode(token: String): Task[JwtClaim] = {
-      ZIO.fromOption(Jwt.decode(token, config.secret, Seq(algo)).toOption)
+      ZIO
+        .fromOption(Jwt.decode(token, config.secret, Seq(algo)).toOption)
         .orElseFail(new Exception("Unable to decode JWT"))
     }
 
@@ -83,10 +119,20 @@ object Authentication {
     }
   }
 
-  def createJwt(credential: UserCredential): RIO[AuthServiceEnv, Response] = ZIO.accessM(_.get.createJwt(credential))
-  def encode(user: String, roles: List[String]): RIO[AuthServiceEnv, String] = ZIO.accessM(_.get.encode(user, roles))
-  def decode(token: String): RIO[AuthServiceEnv, JwtClaim] = ZIO.accessM(_.get.decode(token))
-  def getRoles(jwt: JwtClaim): RIO[AuthServiceEnv, List[String]] = ZIO.accessM(_.get.getRoles(jwt))
+  def createJwt(credential: UserCredential): RIO[AuthServiceEnv, Response] =
+    ZIO.accessM(_.get.createJwt(credential))
+  def encode(user: String, roles: List[String]): RIO[AuthServiceEnv, String] =
+    ZIO.accessM(_.get.encode(user, roles))
+  def decode(token: String): RIO[AuthServiceEnv, JwtClaim] =
+    ZIO.accessM(_.get.decode(token))
+  def getRoles(jwt: JwtClaim): RIO[AuthServiceEnv, List[String]] =
+    ZIO.accessM(_.get.getRoles(jwt))
 
-  val live: ZLayer[Has[UserCredentialRepository.Service] with Has[WebConfig], Throwable, Has[AuthenticationService]] = ZLayer.fromServices[UserCredentialRepository.Service, WebConfig, AuthenticationService]((repo, webconfig) => new Service(repo, webconfig))
+  val live: ZLayer[Has[UserCredentialRepository.Service]
+    with Has[WebConfig], Throwable, Has[AuthenticationService]] =
+    ZLayer.fromServices[
+      UserCredentialRepository.Service,
+      WebConfig,
+      AuthenticationService
+    ]((repo, webconfig) => new Service(repo, webconfig))
 }
